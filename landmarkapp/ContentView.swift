98ea017d6ddfb54c0166d5d8840926fd7113d307
MintSW <mintnimal@gmail.com>
2021-10-13 23:48:12 +0700

    Added detail view

--- landmarkapp/ContentView.swift
+++ landmarkapp/ContentView.swift
@@ -9,16 +9,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Turtle Rock")
-                .font(.title)
+            MapView()
+                .frame(height: 300.0)
             
-            HStack {
-                Text("Joshua Tree National Park")
+            CircleImage()
+                .offset(x: 0, y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Describetive text goes here.")
+                
                 Spacer()
-                Text("California")
+                
             }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
