4b25201555a2778da0ba87bd81651d2c232ec539
MintSW <mintnimal@gmail.com>
2021-10-13 22:59:40 +0700

    Initial Commit

---
+++ landmarkapp/landmarkappApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarkappApp.swift
+//  landmarkapp
+//
+//  Created by Sansern Wuthirat on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct landmarkappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
