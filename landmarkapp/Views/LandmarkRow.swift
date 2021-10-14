0912624c7db1fddc846120157028fa903c9eaed7
MintSW <mintnimal@gmail.com>
2021-10-14 22:42:41 +0700

    Add a star if the LandmarkRow is favorited.

--- landmarkapp/Views/LandmarkRow.swift
+++ landmarkapp/Views/LandmarkRow.swift
@@ -18,11 +18,18 @@ struct LandmarkRow: View {
                 .resizable()
                 .frame(width: 50, height: 50)
             Text(landmark.name)
+            
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
+            
         }
     }
 }
-
+    
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
         Group {
