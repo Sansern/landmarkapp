d52b79389efbb50ab64c3f025b5dd8f8a07014b3
MintSW <mintnimal@gmail.com>
2021-10-19 05:00:15 +0700

    Added profile editor

---
+++ landmarkapp/Views/Profile/ProfileEditor.swift
@@ -0,0 +1,59 @@
+//
+//  ProfileEditor.swift
+//  landmarkapp
+//
+//  Created by Sansern Wuthirat on 10/18/21.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+ 
+    //Set Date Range
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        return min...max
+    }
+    
+    
+    var body: some View {
+        List {
+            VStack {
+                HStack {
+                    Text("Username")
+                        .bold()
+                    Divider()
+                    TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: $profile.username)
+                }
+                
+                Toggle(isOn: $profile.preferNotifications) {
+                    Text("Enable Notifications")
+                        .bold()
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Seasonal Photo")
+                        .bold()
+                    Picker("Seasonal Photo",selection: $profile.seasonalPhoto) {
+                            ForEach(Profile.Season.allCases) { season in
+                                Text(season.rawValue).tag(season)
+                            }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+               
+                DatePicker(selection: $profile.goalDate,in: dateRange, displayedComponents: .date) { Text("Goal Date").bold()
+                }
+                
+            }
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
