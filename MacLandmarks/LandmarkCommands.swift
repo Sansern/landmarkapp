161a35f9d8fb932f5349c53d09417dbad55a8344
MintSW <mintnimal@gmail.com>
2021-11-03 21:57:29 +0700

    Added a build-in  menu command for macOS App for the users that they can toggle sidebar menu

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,13 @@
+//
+//  LandmarkCommands.swift
+//  landmarkapp
+//
+//  Created by Sansern Wuthirat on 11/3/21.
+//
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
--- landmarkapp/Views/Landmarks/LandmarkList.swift
+++ landmarkapp/Views/Landmarks/LandmarkList.swift
@@ -10,40 +10,67 @@ import SwiftUI
 struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
+    @State private var filter = FilterCategory.all
+    
+    enum FilterCategory: String, CaseIterable, Identifiable {
+        // All
+        case all = "All"
+        // Lakes
+        case lakes = "Lakes"
+        // Rivers
+        case riveres = "Rivers"
+        // Moutains
+        case mountains = "Mountains"
+        
+        var id: FilterCategory {self}
+    }
     
 //    Compute a filtered version of the landmarks list by checking the showFavoritesOnly property and each landmark.isFavorite value.
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
-            (!showFavoritesOnly || landmark.isFavorite)
+            (!showFavoritesOnly || landmark.isFavorite) && (filter == .all || filter.rawValue == landmark.category.rawValue)
         }
     }
     
+    var title: String {
+            let title = filter == .all ? "Landmarks" : filter.rawValue
+            return showFavoritesOnly ? "Favorite \(title)" : title
+        }
+    
     var body: some View {
-
         NavigationView {
-            
-            //  The model data’s landmarks array to the List initializer.
-        
             List {
-                Toggle(isOn: $showFavoritesOnly) {
-                    Text("Favorites Only")
-                }
-                .padding(.vertical, 8)
-                
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                         LandmarkRow(landmark: landmark)
+                        #if !os(macOS)
                             .padding(.vertical, 8.0)
+                        #endif
                     }
                 }
-                
-                .navigationTitle("Landmarks")
-                .frame(minWidth: 300)
             }
-            
+            .navigationTitle(title)
+            .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Menu {
+                        Picker("Category", selection: $filter) {
+                            ForEach(FilterCategory.allCases) { category in
+                                Text(category.rawValue).tag(category)
+                            }
+                        }
+                        .pickerStyle(InlinePickerStyle())
+                        
+                        Toggle(isOn: $showFavoritesOnly) {
+                                                    Label("Favorites only", systemImage: "star.fill")
+                        }
+                    } label: {
+                        Label("Filter", systemImage: "slider.horizontal.3")
+                    }
+                }
+            }
+            Text("Select a Landmark")
         }
-        
-        
     }
 }
 
